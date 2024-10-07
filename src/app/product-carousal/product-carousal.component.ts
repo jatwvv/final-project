6a3f8fc0b5d697124b373e19c292a901f85029ae
jatwvv <146978372+jatwvv@users.noreply.github.com>
2024-10-07 08:52:17 +0300

    update

--- src/app/product-carousal/product-carousal.component.ts
+++ src/app/product-carousal/product-carousal.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  AfterViewInit,
+  ElementRef,
+  ViewChild,
+  NgZone,
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
@@ -7,8 +15,55 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
   standalone: true,
   imports: [FontAwesomeModule],
   templateUrl: './product-carousal.component.html',
-  styleUrl: './product-carousal.component.css'
+  styleUrl: './product-carousal.component.css',
 })
-export class ProductCarousalComponent {
+export class ProductCarousalComponent implements AfterViewInit {
+  @ViewChild('carousel') carouselElement!: ElementRef;
+  // leftArrow = faArrowLeft;
   right = faArrowRight;
+
+  private carouselInner!: HTMLElement;
+  private items: HTMLElement[] = [];
+  private itemWidth = 0;
+  private currentIndex = 0;
+
+  constructor(private el: ElementRef, private ngZone: NgZone) {}
+
+  ngAfterViewInit() {
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        this.initializeCarousel();
+      }, 0);
+    });
+  }
+
+  private initializeCarousel() {
+    this.carouselInner = this.el.nativeElement.querySelector('.carousel-inner');
+    this.items = Array.from(this.carouselInner.children) as HTMLElement[];
+
+    if (this.items.length > 0) {
+      this.itemWidth = this.items[0].offsetWidth;
+    }
+
+    this.updateCarousel();
+  }
+
+  private updateCarousel() {
+    const translateX = -this.currentIndex * this.itemWidth;
+    this.carouselInner.style.transform = `translateX(${translateX}px)`;
+  }
+
+  nextSlide() {
+    if (this.currentIndex < this.items.length - 1) {
+      this.currentIndex++;
+      this.updateCarousel();
+    }
+  }
+
+  prevSlide() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updateCarousel();
+    }
+  }
 }
